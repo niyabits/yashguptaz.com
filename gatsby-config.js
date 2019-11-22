af522864859b07a7a44cfa2bd92605e85053ff9e
Yash Gupta <yashguptaofficial@gmail.com>
2019-11-22 11:46:36 +0530

    Added google adsense

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-adsense`,
+      options: {
+        publisherId: `ca-pub-xxxxxxxxxx`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
